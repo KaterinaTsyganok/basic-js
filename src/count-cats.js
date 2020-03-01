88257e344708c9a643dfc9d6cc424b4064f2101f
Katerina Tsyganok <katte002@gmail.com>
2020-03-01 11:33:29 +0300

    feat: add solution what-season

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,17 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+// Your task is to count the cats hidden in the backyard (presented by two-dimensional Array). 
+// Cats hide well, but their ears ("^^") are clearly visible. 
+// Your task is to implement the countCats(backyard) function that will count cats. Good luck!
+// Number of cats found should be number. If no cats found, function should return 0.
+// Write your code in src/count-cats.js.
+  let cat = "^^",
+    count = 0;
+
+  array.forEach(element => {
+    element.forEach(isCat => {
+      if (isCat === cat) count++;
+    })
+  });
+
+  return count;
 };
